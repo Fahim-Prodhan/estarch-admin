e1e2c22cf8fd319e35768f257e44adbea92dcc3d
Fahim-Prodhan <prodhan15-4809@diu.edu.bd>
2024-07-26 15:16:49 +0600

    add: table with pagination in supply List

--- src/pages/Supply/SupplyList/index.js
+++ src/pages/Supply/SupplyList/index.js
@@ -1,12 +1,97 @@
 import React from "react"
-import { Container } from "reactstrap"
+import { Card, CardBody, Col, Container, Row } from "reactstrap"
 import { MdDeleteSweep } from "react-icons/md"
 import { FaEdit } from "react-icons/fa"
 
 //Import Breadcrumb
 import Breadcrumbs from "../../../components/Common/Breadcrumb"
+import { MDBDataTable } from "mdbreact"
+import "./datatables.scss"
+
 
 const Refund = () => {
+  const handleDelete = rowId => {
+    console.log(`Deleting row with id: ${rowId}`)
+    // Add your deletion logic here
+  }
+
+  const data = {
+    columns: [
+      {
+        label: "Image",
+        field: "image",
+        sort: "asc",
+        width: 150,
+      },
+      {
+        label: "Name",
+        field: "name",
+        sort: "asc",
+        width: 270,
+      },
+      {
+        label: "Phone",
+        field: "phone",
+        sort: "asc",
+        width: 200,
+      },
+      {
+        label: "Address",
+        field: "address",
+        sort: "asc",
+        width: 100,
+      },
+      {
+        label: "Status",
+        field: "status",
+        sort: "asc",
+        width: 150,
+      },
+      {
+        label: "Action",
+        field: "action",
+        width: 100,
+      },
+    ],
+    rows: [
+      {
+        image: (
+          <div className="avatar">
+            <div className="w-24 rounded-full">
+              <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+            </div>
+          </div>
+        ),
+        name: <p className="font-bold text-warning">XYX</p>,
+        phone: "017000000",
+        address: "Mirpur",
+        status: (
+          <div className="flex flex-wrap gap-2">
+          <p className="bg-blue-500 text-center text-white p-1 rounded-md w-1">
+            Active
+          </p>
+          <input
+            type="checkbox"
+            className="toggle toggle-info"
+            defaultChecked
+          />
+        </div>
+        ),
+        action: (
+          <div className="flex flex-wrap gap-2">
+                          <button className="text-2xl text-success">
+                            <FaEdit />
+                          </button>
+                          <button className="text-2xl text-error">
+                            <MdDeleteSweep />
+                          </button>
+                        </div>
+        ), // Replace with unique ID or identifier
+      },
+      // Add more rows here...
+    ],
+  }
+
   document.title = "Estarch | Supply List"
 
   return (
@@ -91,14 +176,14 @@ const Refund = () => {
                       </td>
                       <td>01700000</td>
                       <td>Mirpur</td>
-                      <td>
-                        <div className="flex flex-wrap gap-2">
-                          <p className="bg-blue-500 text-center text-white p-1 rounded-md w-">
+                      <td className="w-40">
+                        <div className="flex flex-wrap gap-2 pr-0 mr-0 pl-0 ml-0">
+                          <p className="bg-blue-500 text-center text-white p-1 rounded-md">
                             Active
                           </p>
                           <input
                             type="checkbox"
-                            className="toggle toggle-info"
+                            className="toggle toggle-info "
                             defaultChecked
                           />
                         </div>
@@ -118,6 +203,15 @@ const Refund = () => {
                 </table>
               </div>
             </div>
+            <Row>
+              <Col className="col-12">
+                <Card>
+                  <CardBody>
+                    <MDBDataTable responsive bordered data={data} />
+                  </CardBody>
+                </Card>
+              </Col>
+            </Row>
           </div>
         </Container>
       </div>
